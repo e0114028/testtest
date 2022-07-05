69994726661a0128a81c9726e082948aa89e6871
e0114028 <e0114028@gmail.com>
2022-07-05 15:30:32 +0900

    first first 0705

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,15 @@
+from distutils.command.upload import upload
+from multiprocessing.reduction import register
 from django.db import models
 
-# Create your models here.
+class Friend(models.Model):
+    name = models.CharField(max_length=100)
+    mail = models.EmailField(max_length=200)
+    age = models.IntegerField(default=0)
+    birthday = models.DateField()
+    register_date = models.DateTimeField(help_text='登録日')
+    icon_image = models.ImageField(upload_to='')
+     
+    def __str__(self):
+        return '<Friend:id=' + str(self.id) + ', ' + \
+            self.name + '(' + str(self.age) + ')>'
